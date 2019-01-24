d445637d5804a61449b48ab0513e1b34968885e1
Michael Weber <webermn15@gmail.com>
2019-01-23 21:03:48 -0600

    added some missing babel packages, polyfill to handle async work. cleaned up

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { hot } from 'react-hot-loader'
 
-const App = () => <div style={{margin: "20px auto", fontSize: "48px"}}>Get coding!</div>
+const App = () => <div style={{fontSize: "48px", fontFamily: "Helvetica, sans-serif", fontWeight: "700", display: "flex", flexDirection: "column", alignItems: "center"}}>Get coding!</div>
 
 export default hot(module)(App);
